212447ac7a2090e1996e93ff286aa6d7fe45d53f
Jytesh <44925963+Jytesh@users.noreply.github.com>
2022-02-02 20:39:36 +0530

    added cli
    Signed-off-by: Jytesh <44925963+Jytesh@users.noreply.github.com>

---
+++ cli.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from typing import Optional
+import typer
+from experiment import Experiment
+
+
+def main(file: Path = typer.Argument(..., help="Path to the file to be processed", file_okay=True, exists=True),
+         title: Optional[str] = typer.Option(None),
+         date: Optional[str] = typer.Option(None),
+         manual_output: Optional[str] = typer.Option(None),
+         ):
+    if file is not None and file.is_file():
+        title = title or typer.prompt("Title")
+        date = date or typer.prompt("Date")
+        manual_output = manual_output or False
+        Experiment(str(file), title, date, True, manual_output)
+    else:
+        typer.secho("The file does not exist", fg="red")
+
+
+if __name__ == "__main__":
+    typer.run(main)
